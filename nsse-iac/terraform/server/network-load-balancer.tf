c9353944966999888d87ffc193c29b08540584f5
PabloMelo <pablomelodev@gmail.com>
2026-09-10 22:20:21 -0300

    lint terraform

--- nsse-iac/terraform/server/network-load-balancer.tf
+++ nsse-iac/terraform/server/network-load-balancer.tf
@@ -1,9 +1,9 @@
 resource "aws_lb" "nlb_control_plane" {
-  name = var.network_load_balancer.name
-  internal = var.network_load_balancer.internal # true = so funciona dentro da VPC
+  name               = var.network_load_balancer.name
+  internal           = var.network_load_balancer.internal # true = so funciona dentro da VPC
   load_balancer_type = var.network_load_balancer.load_balancer_type
-  subnets = data.aws_subnets.private_subnets.ids
-  security_groups = [aws_security_group.control_plane]
+  subnets            = data.aws_subnets.private_subnets.ids
+  security_groups    = [aws_security_group.control_plane]
 
   tags = var.tags
 }
